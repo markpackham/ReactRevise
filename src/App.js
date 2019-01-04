c63a3b1883bf2a647134f6361bbef7a24ff3784d
Mark Packham <markpackham1@gmail.com>
2019-01-04 15:41:01 +0000

    reached this point https://youtu.be/sBws8MSXN7A?t=4427

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +15,7 @@ class App extends Component {
             {
                 id: 2,
                 title: 'Brush teeth',
-                completed: false
+                completed: true
             },
             {
                 id: 3,
@@ -23,10 +25,46 @@ class App extends Component {
         ]
     }
 
+    /*
+    Toggle Complete (ticking toggles on off line through on list)
+     */
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        });
+    }
+
+    //Delete task (filter out the one we're deleting)
+    delTodo = (id) => {
+        this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+    }
+
+    //Add a list item
+    addTodo = (title) => {
+        const newTodo = {
+            id: 4,
+            //In ES6 since the key & value are the same we don't have to do title: title
+            title,
+            completed: false
+        }
+        this.setState({todos: [...this.state.todos, newTodo]});
+    }
+
     render() {
         return (
             <div className="App">
-                <Todos todos={this.state.todos}/>
+                <div className="container">
+                    <Header/>
+                    <AddTodo addTodo={this.addTodo}/>
+                    <Todos todos={this.state.todos} markComplete={this.markComplete}
+                           delTodo={this.delTodo}
+                    />
+                </div>
             </div>
         );
     }
